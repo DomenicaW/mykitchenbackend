1425bd2b1ddc3387a571ebe280b561b51f09c88a
dwallace <domenica.wallace5@gmail.com>
2022-11-26 19:03:32 -0500

    added middleware

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//making sure form submits info to server:
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+//import recipe from models folder:
+const Recipe = require('./models/recipes.js')
+
+
+
 app.get('/', (req, res) =>{
   res.send('im working here')
 })
