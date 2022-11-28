1bf296739e3f21c520d5a9945d155faff18e5c55
dwallace <domenica.wallace5@gmail.com>
2022-11-28 14:11:56 -0500

    JUST in case

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
-app.use(cors())
+app.use(cors());
 
 require('./config/db.connection.js')
 
